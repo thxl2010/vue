0e4021233d81f2e631c9778c1a408c4e23e694b9
Duyb <1255248666@qq.com>
2021-07-19 23:25:08 +0800

    docs: vue source code :  entry

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,23 +1,30 @@
-import { initMixin } from './init'
-import { stateMixin } from './state'
-import { renderMixin } from './render'
-import { eventsMixin } from './events'
-import { lifecycleMixin } from './lifecycle'
-import { warn } from '../util/index'
+import { initMixin } from './init';
+import { stateMixin } from './state';
+import { renderMixin } from './render';
+import { eventsMixin } from './events';
+import { lifecycleMixin } from './lifecycle';
+import { warn } from '../util/index';
 
-function Vue (options) {
-  if (process.env.NODE_ENV !== 'production' &&
-    !(this instanceof Vue)
-  ) {
-    warn('Vue is a constructor and should be called with the `new` keyword')
+function Vue(options) {
+  if (process.env.NODE_ENV !== 'production' && !(this instanceof Vue)) {
+    warn('Vue is a constructor and should be called with the `new` keyword');
   }
-  this._init(options)
+  // 调用 _init() 方法
+  this._init(options);
 }
 
-initMixin(Vue)
-stateMixin(Vue)
-eventsMixin(Vue)
-lifecycleMixin(Vue)
-renderMixin(Vue)
+// 注册 vm 的 _init() 方法，初始化 vm
+initMixin(Vue);
+// 注册 vm 的 $data/$props/$set/$delete/$watch
+stateMixin(Vue);
+// 初始化事件相关方法
+// $on/$once/$off/$emit
+eventsMixin(Vue);
+// 初始化生命周期相关的混入方法
+// _update/$forceUpdate/$destroy
+lifecycleMixin(Vue);
+// 混入 render
+// $nextTick/_render
+renderMixin(Vue);
 
-export default Vue
+export default Vue;
--- src/platforms/web/runtime/index.js
+++ src/platforms/web/runtime/index.js
@@ -1,10 +1,10 @@
 /* @flow */
 
-import Vue from 'core/index'
-import config from 'core/config'
-import { extend, noop } from 'shared/util'
-import { mountComponent } from 'core/instance/lifecycle'
-import { devtools, inBrowser } from 'core/util/index'
+import Vue from 'core/index';
+import config from 'core/config';
+import { extend, noop } from 'shared/util';
+import { mountComponent } from 'core/instance/lifecycle';
+import { devtools, inBrowser } from 'core/util/index';
 
 import {
   query,
@@ -12,35 +12,37 @@ import {
   isReservedTag,
   isReservedAttr,
   getTagNamespace,
-  isUnknownElement
-} from 'web/util/index'
+  isUnknownElement,
+} from 'web/util/index';
 
-import { patch } from './patch'
-import platformDirectives from './directives/index'
-import platformComponents from './components/index'
+import { patch } from './patch';
+import platformDirectives from './directives/index';
+import platformComponents from './components/index';
 
 // install platform specific utils
-Vue.config.mustUseProp = mustUseProp
-Vue.config.isReservedTag = isReservedTag
-Vue.config.isReservedAttr = isReservedAttr
-Vue.config.getTagNamespace = getTagNamespace
-Vue.config.isUnknownElement = isUnknownElement
+// 判断是否是关键属性（表单元素的 input/checked/selected/muted）
+// 如果是这些属性，设置 el.props 属性（属性不设置到标签上）
+Vue.config.mustUseProp = mustUseProp;
+Vue.config.isReservedTag = isReservedTag;
+Vue.config.isReservedAttr = isReservedAttr;
+Vue.config.getTagNamespace = getTagNamespace;
+Vue.config.isUnknownElement = isUnknownElement;
 
 // install platform runtime directives & components
-extend(Vue.options.directives, platformDirectives)
-extend(Vue.options.components, platformComponents)
+extend(Vue.options.directives, platformDirectives);
+extend(Vue.options.components, platformComponents);
 
 // install platform patch function
-Vue.prototype.__patch__ = inBrowser ? patch : noop
+Vue.prototype.__patch__ = inBrowser ? patch : noop;
 
 // public mount method
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
-  el = el && inBrowser ? query(el) : undefined
-  return mountComponent(this, el, hydrating)
-}
+  el = el && inBrowser ? query(el) : undefined;
+  return mountComponent(this, el, hydrating);
+};
 
 // devtools global hook
 /* istanbul ignore next */
@@ -48,29 +50,30 @@ if (inBrowser) {
   setTimeout(() => {
     if (config.devtools) {
       if (devtools) {
-        devtools.emit('init', Vue)
+        devtools.emit('init', Vue);
       } else if (
         process.env.NODE_ENV !== 'production' &&
         process.env.NODE_ENV !== 'test'
       ) {
         console[console.info ? 'info' : 'log'](
           'Download the Vue Devtools extension for a better development experience:\n' +
-          'https://github.com/vuejs/vue-devtools'
-        )
+            'https://github.com/vuejs/vue-devtools'
+        );
       }
     }
-    if (process.env.NODE_ENV !== 'production' &&
+    if (
+      process.env.NODE_ENV !== 'production' &&
       process.env.NODE_ENV !== 'test' &&
       config.productionTip !== false &&
       typeof console !== 'undefined'
     ) {
       console[console.info ? 'info' : 'log'](
         `You are running Vue in development mode.\n` +
-        `Make sure to turn on production mode when deploying for production.\n` +
-        `See more tips at https://vuejs.org/guide/deployment.html`
-      )
+          `Make sure to turn on production mode when deploying for production.\n` +
+          `See more tips at https://vuejs.org/guide/deployment.html`
+      );
     }
-  }, 0)
+  }, 0);
 }
 
-export default Vue
+export default Vue;
